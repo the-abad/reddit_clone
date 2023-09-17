4cd00e7272af9c4430c6b5f7bf2359cdba61593f
Zeeshan Abad Siddiqui <abadzeeshan002@gmail.com>
2023-09-17 15:06:43 +0530

    First Commit

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/clientApp";
+import useAuth from "../../hooks/useAuth";
+import Navbar from "../Navbar";
+import AuthModal from "../Modal/Auth";
+import SideNavBar from "../SideNavBar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <SideNavBar />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
