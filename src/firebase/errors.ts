d536df36a6eac918a19a47380c856177defe1a3d
Zeeshan Abad Siddiqui <46226684+the-abad@users.noreply.github.com>
2023-09-18 22:58:56 +0530

    Update errors.ts

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -3,4 +3,5 @@ export const FIREBASE_ERRORS = {
   "Firebase: Error (auth/invalid-login-credentials).": "Invalid email or password",
   "Firebase: Error (auth/user-not-found).": "Invalid email or password",
   "Firebase: Error (auth/wrong-password).": "Invalid email or password",
+  "Firebase: Password should be at least 6 characters (auth/weak-password).": "Password should be at least 6 characters"
 };
