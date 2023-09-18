23075e8ba6b7d9f2a6e0dbfcdb75e27cf4a00d41
Zeeshan Abad Siddiqui <46226684+the-abad@users.noreply.github.com>
2023-09-18 17:24:22 +0530

    Update errors.ts

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -1,7 +1,6 @@
 export const FIREBASE_ERRORS = {
-  "Firebase: Error (auth/email-already-in-use).":
-    "A user with that email already exists",
-
+  "Firebase: Error (auth/email-already-in-use).": "Email already exist!!",
+  "Firebase: Error (auth/invalid-login-credentials).": "Invalid email or password",
   "Firebase: Error (auth/user-not-found).": "Invalid email or password",
   "Firebase: Error (auth/wrong-password).": "Invalid email or password",
 };
